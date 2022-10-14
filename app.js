46542cbee8e264efd4b62b8133a7fe3e7178a567
jpatidar9826 <jpatidar9826@gmail.com>
2022-10-14 18:32:19 +0530

    hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  const bodyParser = require("body-parser");
  const ejs = require("ejs");
  const mongoose = require("mongoose");
- const encrypt = require("mongoose-encryption");
+ const md5 = require("md5");
 
  const app = express();
 
@@ -22,7 +22,7 @@
  });
 
 
- userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
  const User = new mongoose.model("User",userSchema);
 
@@ -41,7 +41,7 @@
  app.post("/login",function(req,res){
 
    const userName = req.body.username;
-   const userPass = req.body.password;
+   const userPass = md5(req.body.password);
 
    User.findOne({email: userName},function(err,result){
      if (err) {
@@ -61,7 +61,7 @@
 
    const newUser = new User({
      email: req.body.username,
-     password: req.body.password
+     password: md5(req.body.password)
    });
 
    newUser.save(function(err){
